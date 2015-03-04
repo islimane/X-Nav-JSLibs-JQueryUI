882721bb5b5c6df4fc3c009bc41761502980323d
Ismael Slimane <isma.bled777@gmail.com>
2015-03-04 22:02:57 +0100

    Implemented some graphic functions

--- widgets.js
+++ widgets.js
@@ -1,34 +1,67 @@
 $(document).ready(function() {
 	var dropped = false;
+	var numOfFolders = 2;
 	var folders = [];
+	var numOfFiles = 5;
 	var files = [];
-	var currentdate = new Date(); 
-	var day = currentdate.getDate();
-	if(day<10)
-		day = "0" + day;
-	var month = currentdate.getMonth()+1;
-	if(month<10)
-		month = "0" + month;
-    var year = currentdate.getFullYear();
-	var datetime = month + "/" + day + "/"  + year;
+	var datetime = updateDate();
 	$("#date").datepicker();
 	$("#date").val(datetime);
-    $( "#menu" ).menu();
+    $("#menu").menu();
+	function setDraggable(){
+		$(".draggable").draggable({
+			revert: function( event, ui ){
+				$(this).data("uiDraggable").originalPosition = {
+				    top : 0,
+				    left : 0
+				};
+				if(event){
+					console.log("FADE");
+					$(this).effect("fade");
+					$(this).remove();
+					numOfFiles--;
+				}
+				return !event;
+			}
+		});
+	};
+	setDraggable();
 
-	$( ".draggable" ).draggable({
-		revert: function( event, ui ){
-			$(this).data("uiDraggable").originalPosition = {
-		        top : 0,
-		        left : 0
-			};
-			return !event;
-		}
+	function setDroppable(){
+		$(".droppable").droppable({
+			drop: function( event, ui ) {
+				//$( this ).addClass("ui-state-highlight").find("p").html( "Dropped!" );
+				dropped = true;
+			}
+		});
+	};
+	setDroppable();
+
+	$("#newFile").click(function() {
+		numOfFiles++;
+		var HtmlElement = '<img class="draggable effect" src="images/windows_icons/086.png">';
+		$("#files").append($(HtmlElement));
+		// it's necesary to set the new img as draggable
+		setDraggable();
 	});
 
-	$( ".droppable" ).droppable({
-		drop: function( event, ui ) {
-			//$( this ).addClass("ui-state-highlight").find( "p" ).html( "Dropped!" );
-			dropped = true;
-		}
+	$("#newFolder").click(function() {
+		numOfFolders++;
+		var HtmlElement = '<img class="droppable" src="images/windows_icons/218.png">';
+		$("#folders").append($(HtmlElement));
+		// it's necesary to set the new img as draggable
+		setDroppable();
 	});
+
+	function updateDate(){
+		var currentdate = new Date(); 
+		var day = currentdate.getDate();
+		if(day<10)
+			day = "0" + day;
+		var month = currentdate.getMonth()+1;
+		if(month<10)
+			month = "0" + month;
+		var year = currentdate.getFullYear();
+		return (month + "/" + day + "/" + year);
+	};
 });
